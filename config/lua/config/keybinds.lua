ea6e2d8e7f89641d1850adcc818181da9f7fff23
Hunter2718 <hunter_2718@yahoo.com>
2025-06-16 12:39:52 -0500

    Update the config for 2025-05-16

---
+++ config/lua/config/keybinds.lua
@@ -0,0 +1,4 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>md", vim.cmd.MarkdownPreview)
+vim.keymap.set("n", "<leader>mds", vim.cmd.MarkdownPreviewStop)
